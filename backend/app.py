91ec34a89031adf20fdf0583a5942cf5dc2b2453
taddymason <tdytomsawyer@protonmail.com>
2025-04-29 14:31:20 -0700

    docker files

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,12 @@ app = FastAPI(title="Sentiment Analysis API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  
+    allow_origins=[
+        "http://localhost:5173", 
+        "http://localhost:80",    
+        "http://localhost",      
+        "http://frontend",        
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
